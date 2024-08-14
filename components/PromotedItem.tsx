0a363ec76da038d0f5109cb2defa88504c7caa2e
Berke Bakar <88384613+berke-bakar@users.noreply.github.com>
2024-08-14 15:06:46 +0300

    -> Promoted item card implementd

---
+++ components/PromotedItem.tsx
@@ -0,0 +1,72 @@
+import { cn } from "@/utils/util";
+import { StaticImageData } from "next/image";
+import React from "react";
+
+type PromotedItemProps = {
+  productName: string | React.ReactElement;
+  productDescription?: string;
+  align?: "left" | "right";
+  bgImage: StaticImageData;
+  buttonClass?: string;
+  separated?: boolean;
+  className?: string;
+};
+
+export default function PromotedItem({
+  productName,
+  productDescription,
+  align = "left",
+  bgImage,
+  buttonClass = "btn-1",
+  separated = false,
+  className,
+}: PromotedItemProps) {
+  console.log(bgImage.src);
+
+  const ProductNameEl =
+    typeof productName === "string" ? (
+      <h4 className="uppercase">{productName}</h4>
+    ) : (
+      productName
+    );
+
+  return (
+    <div
+      className={cn(
+        "xl:w-[1110px] xl:h-[320px] rounded-lg mx-auto flex items-center",
+        {
+          "justify-end": align === "right" && !separated,
+          "gap-[30px]": separated,
+          "px-[95px]": !separated,
+        },
+        className
+      )}
+      style={{
+        backgroundImage: !separated ? `url(${bgImage.src})` : "",
+        height: bgImage.height,
+      }}
+    >
+      {separated && (
+        <div
+          className="max-h-full rounded-lg"
+          style={{
+            backgroundImage: `url(${bgImage.src})`,
+            height: bgImage.height,
+            width: bgImage.width,
+          }}
+        ></div>
+      )}
+
+      <div
+        className={cn("flex flex-col gap-[32px] basis-[350px]", {
+          "bg-slate-medium px-[95px] h-full justify-center rounded-lg grow":
+            separated,
+        })}
+      >
+        {ProductNameEl}
+        {productDescription && <p>{productDescription}</p>}
+        <button className={cn("uppercase", buttonClass)}>See Product</button>
+      </div>
+    </div>
+  );
+}
